369cf4d400fc050882818ecabfe4d342975b6bbd
dave123online <dave.capo-chichi@epitech.eu>
2026-09-04 20:38:03 +0100

    add seo optimisations

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = "core"
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path("", views.accueil, name="accueil"),
@@ -11,4 +13,6 @@ urlpatterns = [
     path("ou-nous-trouver/", views.ou_nous_trouver, name="ou_nous_trouver"),
     path("chatbot/message/", views.chatbot_message, name="chatbot_message"),
     path("politique-confidentialite/", views.politique_confidentialite, name="politique_confidentialite"),
+    path("sitemap.xml", TemplateView.as_view(template_name="core/sitemap.xml", content_type="application/xml"), name="sitemap"),
+    path("robots.txt", TemplateView.as_view(template_name="core/robots.txt", content_type="text/plain"), name="robots"),
 ]
